a825d240b5175036f57aa68d16bdc4eb8745fce1
Shatsui <ShatsuiMc@gmail.com>
2014-01-15 18:11:55 +0100

    Add Starting Logger

--- src/fr/shatsui/pong/GameController.java
+++ src/fr/shatsui/pong/GameController.java
@@ -1,19 +1,23 @@
 package fr.shatsui.pong;
 
 import org.apache.log4j.Logger;
+import static org.lwjgl.opengl.GL11.*;
 
-/**
- * Created by Shatsui on 15/01/14.
- */
 public class GameController {
 
     private Game game;
     private Logger logger;
     private GameState state;
 
+    private static final String WINDOW_NAME = "Snake";
+    private static final String VERSION = "0.1";
+
     public GameController(Game game) {
         this.game = game;
         this.logger = Logger.getLogger(Game.class);
+        logger.info("OS : " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ") " + System.getProperty("os.arch"));
+        logger.info("Java : " + System.getProperty("java.version") + " / " + System.getProperty("java.vendor"));
+        logger.info("Started new Game v" + VERSION + " with OpenGL : " + GL_VERSION);
     }
 
     public void switchTo(GameState state) {
